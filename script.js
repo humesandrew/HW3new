f52ef0de1776b176dfb45ff7253c63271f2f8240
Andy Humes <andrew.humes@du.edu>
2021-11-06 21:44:11 -0600

    after collabs

--- script.js
+++ script.js
@@ -1,25 +1,32 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var generate = document.querySelector("generate");
+var finalPassword;
 
 
-const uppercase = ["A", "B", "C", "D", "E", "F"]
-const lowercase =["a", "b", "c", "d", "e", "f"]
-const characts = ["!", "@", "#", "$", "%", "^"]
-const number = ["0", "1", "2", "3", "4", "5", "6"]
+
+var uppercase = ["A", "B", "C", "D", "E", "F"];
+var lowercase =["a", "b", "c", "d", "e", "f"];
+var characts = ["!", "@", "#", "$", "%", "^"];
+var numeric = ["0", "1", "2", "3", "4", "5", "6"];
+var hasUpperCase;
+var hasLowerCase;
+var hasCharacts;
+var hasNumber; 
+
+var charactersEntered;
 
 
-function genPassword () {
+function generatePassword () {
   var length = parseInt(prompt("How many characters would you like?"), 10);
 if (Number.isNaN(length)) {
   alert("Please enter a valid number.");
-  return null;
+ generatePassword();
 }
 if (length < 8 || length > 128) {
   alert("Length is shorter than 8 or longer than 128 characters.");
-  return null; 
+  generatePassword(); 
 }
-
+else {
 var hasUpperCase = confirm("Would you like uppercase?");
 var hasLowerCase = confirm("Would you like lowercase?");
 var hasCharacts = confirm("Would you like characters?");
@@ -27,71 +34,61 @@ var hasNumber = confirm("Would you like numbers?");
 
 if (hasUpperCase === false && hasLowerCase === false && hasCharacts === false && hasNumber === false) {
   alert("Please select at least one parameter.");
-  return null; 
+  generatePassword();
 }
 
-if (hasUpperCase === true && hasLowerCase === false && hasCharacts === false && hasNumber === false) {
-  alert("Your password will have Upper Case letters.");
-
-}
-
-if (hasUpperCase && hasLowerCase && hasCharacts === false && hasNumber === false) {
-  alert("Your password will have Upper Case and Lower Case letters.");
-}
-
-if (hasUpperCase && hasLowerCase && hasCharacts && hasNumber === false) {
-  alert("Your password will have Upper Case, Lower Case, and Characters.");
-}
-
-if (hasUpperCase && hasLowerCase && hasCharacts && hasNumber) {
-  alert("Your password will have Upper Case, Lower Case, Characters, and Numbers.");
+//selected all 4//
+else if (hasUpperCase && hasLowerCase && hasCharacts && hasNumber) {
+  
+  charactersEntered = numeric.concat(uppercase, lowercase, characts);
 
 }
 
-
-var choices = {
-  length: length,
-  hasUpperCase: hasUpperCase,
-  hasLowerCase: hasLowerCase,
-  hasCharacts: hasCharacts,
-  hasNumber: hasNumber,
-}; 
-return choices;
+//selected 3 options//
+else if (hasUpperCase && hasLowerCase && hasCharacts) {
+  charactersEntered = uppercase.concat(lowercase, characts);
+  
 }
+
+//selected 2 optiosn//
+else if (hasUpperCase && hasLowerCase) {
+  charactersEntered = uppercase.concat(lowercase);
+}
+
+//selected 1 option - just uppercase//
+else if (hasUpperCase) {
+  charactersEntered = uppercase;
+
+}
+
+//ignore this //
+
 //Ignoring the parameter choices for now////
 
-var password = "";
-for (var i = 0; i <=length; i++) 
-{var randomNumber = Math.floor(Math.random() * uppercase.length)
-password += uppercase.substring(randomNumber, randomNumber +1);
+
+
+var tempPassword = [];
+
+for (let i = 0; i < length; i++) {
+  var finalCharacters = charactersEntered[Math.floor(Math.random() * charactersEntered.length)];
+  tempPassword.push(finalCharacters);
 }
-
-function getRandom() {
-  
-
-
-
-
+var tempPassword2 = tempPassword.join('');
+finalPassword = tempPassword2;
+return tempPassword;
 }
-function generatePassword() {
-  var choices = genPassword() 
-
-}
+};
 
 
-// Write password to the #password input
+
+// Write password to the #password input//
 function writePassword() {
-  var password = generatePassword() 
+  generatePassword()
+  document.getElementById('password').textContent = finalPassword;
 
+ 
 }
 
-  
 
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-
-
-// Add event listener to generate button
+// Add event listener to generate button//
 generateBtn.addEventListener("click", writePassword);
